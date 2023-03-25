f81639ed0f4dc9611d1bb7d8f3141aa1306308af
fredboys <freddieboys@hotmail.co.uk>
2023-03-25 11:08:31 +0000

    logged in vs logged out

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import { Link, useHistory } from "react-router-dom";
 
 import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
@@ -7,8 +7,11 @@ import axios from "axios";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
 
 function SignInForm() {
+    const setCurrentUser = useContext(SetCurrentUserContext) 
+
     const [signInData, setSignInData] = useState({
         username: '',
         password: '',
@@ -27,7 +30,8 @@ function SignInForm() {
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            await axios.post('/dj-rest-auth/login/', signInData);
+            const {data} = await axios.post('/dj-rest-auth/login/', signInData);
+            setCurrentUser(data.user)
             history.push('/');
         } catch(err){
             setErrors(err.response?.data)
